82e112006cf6de9d448cd2e377e127388d38c8ed
Sw1dy <145874901+Sw1dy@users.noreply.github.com>
2024-12-16 01:02:15 +0300

    Add files via upload

--- graph.py
+++ graph.py
@@ -1,23 +1,8 @@
 import os
 import subprocess
+import argparse
 from git import Repo
 from datetime import datetime, timedelta
-
-# Путь к программе для визуализации графов (PlantUML)
-plantuml_path = "F:/Projects/DzConf_2/PlantUML/plantuml-1.2024.8.jar"
-
-# Путь к анализируемому репозиторию
-repo_path = "F:/Projects/DzConf_2/Dz_local"
-
-# Путь к файлу-результату в виде кода
-output_dir = "F:/Projects/DzConf_2/output"
-output_file_path = os.path.join(output_dir, "graph.puml")
-
-# Создание директории, если она не существует
-os.makedirs(output_dir, exist_ok=True)
-
-# Дата коммитов в репозитории (например, за последние 30 дней)
-date_threshold = datetime.now() - timedelta(days=30)
 
 def generate_plantuml_code(repo_path, output_file_path, date_threshold):
     repo = Repo(repo_path)
@@ -35,8 +20,34 @@ def visualize_graph(plantuml_path, output_file_path):
     output_image_path = output_file_path.replace(".puml", ".png")
     subprocess.run(["java", "-jar", plantuml_path, output_file_path])
 
-# Генерация PlantUML кода
-generate_plantuml_code(repo_path, output_file_path, date_threshold)
+def main():
+    parser = argparse.ArgumentParser(description="Generate and visualize commit graph using PlantUML.")
+    parser.add_argument("plantuml_path", help="Path to the PlantUML JAR file.")
+    parser.add_argument("repo_path", help="Path to the repository.")
+    parser.add_argument("output_dir", help="Path to the output directory.")
+    parser.add_argument("--days", type=int, default=30, help="Number of days for commit threshold.")
 
-# Визуализация графа
-visualize_graph(plantuml_path, output_file_path)
+    args = parser.parse_args()
+
+    plantuml_path = args.plantuml_path
+    repo_path = args.repo_path
+    output_dir = args.output_dir
+    days = args.days
+
+    # Путь к файлу-результату в виде кода
+    output_file_path = os.path.join(output_dir, "graph.puml")
+
+    # Создание директории, если она не существует
+    os.makedirs(output_dir, exist_ok=True)
+
+    # Дата коммитов в репозитории (например, за последние 30 дней)
+    date_threshold = datetime.now() - timedelta(days=days)
+
+    # Генерация PlantUML кода
+    generate_plantuml_code(repo_path, output_file_path, date_threshold)
+
+    # Визуализация графа
+    visualize_graph(plantuml_path, output_file_path)
+
+if __name__ == "__main__":
+    main()
